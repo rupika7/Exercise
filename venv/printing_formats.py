3b9d29c0cfa4245d4d7cea2039d2e10e9c08228c
RUPA <pikarupika7@gmail.com>
2021-06-11 12:12:47 +0545

    desk added

--- venv/printing_formats.py
+++
@@ -1,8 +0,0 @@
-name = input("enter your name:")
-age = int(input("enter your age:"))
-
-print("hello my name is " + name + " and i am " + str (age) + "years old")
-print("hello my name is", name, "and i am", age, "years old")
-print("hello my name is %s and i am %d years old" % (name, age))
-print("hello my name is {} and i am {} years old".format(name, age))
-print(f"hello my name is {name} and i am {age} years old")
