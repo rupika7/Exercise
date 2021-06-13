ffdcba764253cfc8503820a8807a5396d356e3b3
RUPA <pikarupika7@gmail.com>
2021-06-13 20:15:01 +0545

    area of circle

---
+++ eighth.py
@@ -0,0 +1,6 @@
+'''
+Write a Python program which accepts the radius of a circle from the user and compute the area. (area of circle = PI * r2)
+'''
+radius = float(input("enter the radius of the circle"))
+area_of_circle = 22/7 * radius**2
+print(f"the area of circle is {area_of_circle}")
