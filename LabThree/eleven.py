5114b2a4220a2dc85c26f919fbf4ccb1eaf51f67
RUPA <pikarupika7@gmail.com>
2021-07-04 21:56:56 +0545

    armstrong

--- LabThree/eleven.py
+++ LabThree/eleven.py
@@ -3,7 +3,7 @@ Write a python program to check whether the number is Armstrong number or not us
 [Hint: 153 - 1*1*1 + 5*5*5 + 3*3*3]
 '''
 def armstrong(digit):
-    sum = 0  # initialize sum
+    sum = 0
     # find the sum of the cube of each digit
     n = num
     while n > 0:
@@ -11,11 +11,10 @@ def armstrong(digit):
         sum += digit ** 3
         n //= 10
     # display the result
-    if num == sum:
+    if num==sum:
         return True
     else:
         return False
-
 
 # take input from the user
 num = int(input("Enter a number: "))
