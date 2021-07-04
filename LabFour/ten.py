cd20476d79d31f1150a89bd4b7fd17135af0dd3b
RUPA <pikarupika7@gmail.com>
2021-07-04 22:18:50 +0545

    count dig letter

--- LabFour/ten.py
+++ LabFour/ten.py
@@ -7,7 +7,7 @@ count_digit=0
 count_letter=0
 for i in string:
     if i.isdigit():
-        count_digit+=1
+        count_digit+= 1
     elif i.isalpha():
         count_letter+=1
 print(f"the number of digits: {count_digit} the number of letters: {count_digit}")
