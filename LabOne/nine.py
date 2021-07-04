24a8531bef2137a68f43344d21e63b2d2cd46de7
RUPA <pikarupika7@gmail.com>
2021-07-04 22:38:42 +0545

    first positive num

---
+++ LabOne/nine.py
@@ -0,0 +1,7 @@
+'''
+Write a python program to find sum of the first n positive integers.
+sum = (n*(n+1))/2
+'''
+n = int(input("enter first positive number"))
+sum = (n*(n+1)) / 2
+print(f"the sum of the first n positive integers is {sum}")
