485d0b3d34f87513d4e5d7c4f35b82d7212b717f
RUPA <pikarupika7@gmail.com>
2021-07-04 22:14:23 +0545

    temp in c

--- LabFour/two.py
+++ LabFour/two.py
@@ -6,5 +6,5 @@ fahrenheit.
 def temp():
     F = float(input("enter temperature in Fahrenheit: "))
     C = (5/9)*(F - 32)
-    print(f"the temperature in Celcius is {C}")
+    print(f"the temperature in Celcius is: {C}")
 temp()
