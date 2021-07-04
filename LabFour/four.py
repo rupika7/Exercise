65c1563f201a47a6e770b66355d9516d4fb7a921
RUPA <pikarupika7@gmail.com>
2021-07-04 22:17:32 +0545

    star pattern

--- LabFour/four.py
+++ LabFour/four.py
@@ -18,6 +18,6 @@ def pattern(n):
         print(" ")
     for i in range(n, -1, -1):
         for j in range(0, i + 1):
-            print("* ", end="")
+            print("* ", end ="")
         print(" ")
 pattern(5)
