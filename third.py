e05c6b5cefacdcab31825fa2de6a658879f9715c
RUPA <pikarupika7@gmail.com>
2021-07-04 21:46:52 +0545

    sum 3 num

--- third.py
+++ third.py
@@ -7,5 +7,4 @@ num1 = int(input("enter the first value: "))
 num2 = int(input("enter the second value: "))
 num3 = int(input("enter the third value: "))
 sum = num1 + num2 + num3
-
 print(f"The sum of given three numbers is {sum}")
