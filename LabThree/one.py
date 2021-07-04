f705fb78d7ed0f72d5e57eeda5f89419b86f707a
RUPA <pikarupika7@gmail.com>
2021-07-04 20:21:48 +0545

    maximum num

--- LabThree/one.py
+++ LabThree/one.py
@@ -2,9 +2,9 @@
  Write a Python function to find the Max of three numbers
 '''
 def max():
-    a=int(input("enter number"))
-    b=int(input("enter number"))
-    c=int(input("enter number"))
+    a= int(input("enter number"))
+    b= int(input("enter number"))
+    c= int(input("enter number"))
     if a>b and a>c:
         print(f"the max number is {a}")
     elif b>a and b>c:
