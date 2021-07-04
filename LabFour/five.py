0ea9ac2430ee602cdc60d4805fac9b47f37443af
RUPA <pikarupika7@gmail.com>
2021-07-04 20:11:45 +0545

    reverse

--- LabFour/five.py
+++ LabFour/five.py
@@ -2,7 +2,7 @@
 write a Python program that accepts a word from the user and reverse it.
 '''
 def rev(word):
-    reverse= word[::-1]
+    reverse = word[::-1]
     print(f"the reverse of the word is {reverse} ")
 word=input("enter a word: ")
 rev(word)
